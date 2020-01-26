b133fa9dc31ac8592f42519276634cb15a5d3643
Rufanko <rufanko@mail.ru>
2020-01-26 08:33:10 +0300

    Create answers table

--- db/migrate/20200126051726_create_answers.rb
+++ db/migrate/20200126051726_create_answers.rb
@@ -1,6 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
+      t.text :body
+      t.references :question, foreign_key: true, null: false
 
       t.timestamps
     end
