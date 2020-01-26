3e9aaa96a2f02f93fd8c6cf2b29d4df96e5eb3f6
Rufanko <rufanko@mail.ru>
2020-01-26 08:27:45 +0300

    Add associations

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   describe 'associations' do
-    it { should belong_to(:question)}
+    it { should belong_to(:question) }
   end
 
   describe 'validation' do
