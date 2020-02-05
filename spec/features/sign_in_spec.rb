cbbfb898aee54352d4e71f1c190f526f9ad6749e
Rufanko <rufanko@mail.ru>
2020-02-05 11:49:01 +0300

    Add sign in tests

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -5,15 +5,26 @@ feature 'User can sign in',  %q{
   As an unauthenticated user
   I'd like to able to sign in
 } do
+
+  given(:user) {User.create!(email: 'asd@test.com', password '12345679' )}
+
+  background { visit new_user_session_path}
+
   scenario 'Registered user tries to sign in' do
-    User.create!(email: 'asd@test.com', password: '1234567')
+    User.create!(email: 'asd@test.com', password: '12345679')
 
-    visit new_user_session_path
-    fill_in 'Email',  with: 'asd@test.com'
-    fill_in 'Password', with: '1234567'
+    fill_in 'Email',  with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
 
-    expect(page).to have_content 'Signed in succesfully.'
+    expect(page).to have_content 'Signed in successfully.'
   end
 
-  scenario 'Unregistered user tries to sign in'
+  scenario 'Unregistered user tries to sign in' do
+    fill_in 'Email',  with: 'wrong@test.com'
+    fill_in 'Password', with: '12345679'
+    click_button 'Log in'
+    expect(page).to have_content 'Invalid Email or password.'
+
+  end
 end
