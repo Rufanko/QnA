0cf1d51f6641d144eee0910c50f0923b77b1d07d
Rufanko <rufanko@mail.ru>
2020-02-05 09:19:44 +0300

    Generate User model

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'Use can sign in',  %q{
+  In order to ask questions
+  As an unauthenticated user
+  I'd like to able to sign in
+} do
+  scenario 'Registered user tries to sign in' do
+    User.create!(email: 'asd@test.com', password: '1234567')
+
+    visit '/login'
+    fill_in 'Email',  with: 'asd@test.com'
+    fill_in 'Password', with '1234567'
+
+    expect(page).to have_content 'Signed in succesfully.'
+  end
+
+  scenario 'Unregistered user tries to sign in'
+end
