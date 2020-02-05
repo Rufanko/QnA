85491e301fbcd4cad947e16bad2c47e6fb69c11a
Rufanko <rufanko@mail.ru>
2020-02-05 09:50:52 +0300

    Install webpacker

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'Use can sign in',  %q{
+feature 'User can sign in',  %q{
   In order to ask questions
   As an unauthenticated user
   I'd like to able to sign in
@@ -8,9 +8,9 @@ feature 'Use can sign in',  %q{
   scenario 'Registered user tries to sign in' do
     User.create!(email: 'asd@test.com', password: '1234567')
 
-    visit '/login'
+    visit new_user_session_path
     fill_in 'Email',  with: 'asd@test.com'
-    fill_in 'Password', with '1234567'
+    fill_in 'Password', with: '1234567'
 
     expect(page).to have_content 'Signed in succesfully.'
   end
