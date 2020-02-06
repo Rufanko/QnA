e2ad078a67fb4f8011862f261e7ec519a65fa02b
Rufanko <rufanko@mail.ru>
2020-02-06 04:18:14 +0300

    Refactor create_spec

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -4,17 +4,20 @@ feature 'User can create a question', %q{
   As an authenticated user
   I'd like to be able to ask the question
 } do
-  given(:user) {User.create!(email: 'asd@test.com', password: '12345679')}
-  background { visit new_user_session_path}
+  given(:user) {create(:user)}
+describe 'Authenticated user' do
 
-  scenario 'Authenticated user asks a question' do
+
+  background do
+    visit new_user_session_path
     fill_in 'Email',  with: user.email
     fill_in 'Password', with: user.password
     click_button 'Log in'
-
     visit questions_path
     click_on 'Ask question'
+  end
 
+  scenario 'Authenticated user asks a question' do
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: 'content'
     click_on 'Ask'
@@ -22,8 +25,16 @@ feature 'User can create a question', %q{
     expect(page).to have_content 'Your question successfully created.'
     expect(page).to have_content 'Test question'
     expect(page).to have_content 'content'
-
   end
+
+  scenario 'Authenticated user asks a question with errors' do
+    visit questions_path
+    click_on 'Ask question'
+    click_on 'Ask'
+    expect(page).to have_content "Title can't be blank"
+  end
+end
+
 
   scenario 'Unathenticated user asks a question' do
     visit questions_path
@@ -31,21 +42,6 @@ feature 'User can create a question', %q{
 
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
-
-  scenario 'Authenticated user asks a question with errors' do
-    fill_in 'Email',  with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
-
-    visit questions_path
-    click_on 'Ask question'
-
-    click_on 'Ask'
-
-    expect(page).to have_content "Title can't be blank"
-
-  end
-
 
 
 end
--- spec/support/contoller_helpers.rb
+++ spec/support/contoller_helpers.rb
@@ -1,5 +1,6 @@
 module ControllerHelpers
-  def login(user)     @request.env['devise.mapping'] = Devise.mappings[:user]
+  def login(user)
+    @request.env['devise.mapping'] = Devise.mappings[:user]
     sign_in(user)
   end
 end
