a4740411c1ad26992d377b996ec76a311d36960c
Rufanko <rufanko@mail.ru>
2020-02-12 10:04:07 +0300

    Change associations names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :authored_questions, class_name: 'Question', foreign_key: 'author_id', dependent: :nullify
-  has_many :authored_answers, class_name: 'Answer', foreign_key: 'author_id', dependent: :nullify
+  has_many :questions, class_name: 'Question', foreign_key: 'author_id', dependent: :nullify
+  has_many :answers, class_name: 'Answer', foreign_key: 'author_id', dependent: :nullify
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
