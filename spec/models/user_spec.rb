ebc152344c06f022d0cf1a3e149cd2d359a24848
Rufanko <rufanko@mail.ru>
2020-02-12 11:41:02 +0300

    Add test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,22 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :password }
   end
 
+  describe 'associations' do
+    it { should have_many(:answers).dependent(:nullify)}
+    it { should have_many(:questions).dependent(:nullify)}
+  end
+
+  describe '#author?' do
+    let(:user) {create (:user)}
+    let(:question_without_author) {create(:question)}
+    let(:question) {create(:question, author: user)}
+    it 'should correctly check the authorship' do
+      expect(user.author?(question)).to eq true
+      expect(user.author?(question_without_author)).to eq false
+    end
+  end
+
+
+
+
 end
